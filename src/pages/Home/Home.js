e7259574101c6df220ea5e51902260d6e356c94e
SohelTanbir <soheltanbir19@gmail.com>
2022-07-23 14:56:09 +0600

    stramers section completed and a splide slider

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
 import Introduction from '../../components/Introduction/Introduction';
-
+import Stramer from '../../components/Streamer/Stramer';
 
 const Home = () => {
     return (
         <div id='home'>
             <Banner/>
             <Introduction/>
-            
+            <Stramer/>
         </div>
     );
 };
