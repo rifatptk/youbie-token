0faa91e84e4c47c7a70fd7e8d11ced1b017e1cbe
SohelTanbir <soheltanbir19@gmail.com>
2022-07-24 22:38:36 +0600

    added swiper slider on betting section

--- src/components/Betting/Betting.js
+++ src/components/Betting/Betting.js
@@ -4,35 +4,14 @@ import trend1 from '../../assets/images/trend-1.png'
 import trend2 from '../../assets/images/trend-2.png'
 import trend3 from '../../assets/images/trend-3.png'
 import trend4 from '../../assets/images/trend-4.png'
-import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
+import BettingSlider from './BettingSlider';
 
 
 
 
 const Betting = () => {
-    const bettingSliderConfig = {
-        type: "fade",
-        rewind: true,
-        pagination: false,
-        arrows: false,
-        perPage: 1,
-    }
-    const thumbnaiSliderConfig ={
-        fixedWidth: 146,
-      fixedHeight: 96,
-      gap: 12,
-      rewind: true,
-      pagination: false,
-      isNavigation: true,
-      breakpoints: {
-        600: {
-          fixedWidth: 60,
-          gap: 5,
-          fixedHeight: 44,
-        },
-      }
-    } 
+
 
     return (
         <div className='betting-section'>
@@ -107,184 +86,7 @@ const Betting = () => {
                 <h3 className="heading__3">Coming soon...</h3>
               </div>
               <div className="col-xl-6" data-aos="fade-up">
-                <section
-                  id="betting-main-carousel"
-                  className="betting-main-carousel"
-                  aria-label="The carousel with thumbnails. Selecting a thumbnail will change the Beautiful Gallery carousel."
-                >
-                  <div className="splide__track">
-                    <ul className="splide__list">
-                      <li className="splide__slide">
-                        <img
-                          src={trend1}
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                      <li className="splide__slide">
-                        <img
-                          src={trend2}
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                      <li className="splide__slide">
-                        <img
-                          src={trend3}
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                      <li className="splide__slide">
-                        <img
-                          src={trend3}
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                      <li className="splide__slide">
-                        <img
-                          src="./assets/images/trend-1.png"
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                      <li className="splide__slide">
-                        <img
-                          src="./assets/images/trend-2.png"
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                      <li className="splide__slide">
-                        <img
-                          src="./assets/images/trend-3.png"
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                    </ul>
-                  </div>
-                </section>
-
-                <section
-                  id="betting-thumbnail-carousel"
-                  className="splide betting-thumbnail-carousel"
-                  aria-label="The carousel with thumbnails. Selecting a thumbnail will change the Beautiful Gallery carousel."
-                >
-                  <div className="splide__arrows">
-                    <button className="splide__arrow splide__arrow--prev">
-                      <svg
-                        version="1.2"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="18"
-                        height="12"
-                        style={{transform:'rotate(90deg)'}}
-                      >
-                        <path
-                          d="m.9.3 8.1 6 8.1-6v5.1l-8.1 6-8.1-6z"
-                            fill='#fff'
-                        />
-                      </svg>
-                    </button>
-                    <button className="splide__arrow splide__arrow--next">
-                      <svg
-                        version="1.2"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="18"
-                        height="12"
-                        style={{transform: `rotate(-90deg)`}}
-                      >
-                        <path
-                          d="m.9.3 8.1 6 8.1-6v5.1l-8.1 6-8.1-6z"
-                          fill="#fff"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-
-                  <div className="splide__track">
-                    <ul className="splide__list">
-                      <li className="splide__slide">
-                        <img
-                          src={trend1}
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                      <li className="splide__slide">
-                        <img
-                          src={trend2}
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                      <li className="splide__slide">
-                        <img
-                          src={trend3}
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                      <li className="splide__slide">
-                        <img
-                          src={trend4}
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                      <li className="splide__slide">
-                        <img
-                          src={trend1}
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                      <li className="splide__slide">
-                        <img
-                          src={trend2}
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                      <li className="splide__slide">
-                        <img
-                          src={trend3}
-                          alt="slide"
-                          width="520"
-                          height="400"
-                          className="slide__img"
-                        />
-                      </li>
-                    </ul>
-                  </div>
-                </section>
+                  <BettingSlider/>
               </div>
             </div>
           </div>
---
+++ src/components/Betting/BettingSlider.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import './BettingSlider.css'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+// import required modules
+import { FreeMode, Navigation, Thumbs } from "swiper";
+
+// import images
+import trend1 from '../../assets/images/trend-1.png'
+import trend2 from '../../assets/images/trend-2.png'
+import trend3 from '../../assets/images/trend-3.png'
+import trend4 from '../../assets/images/trend-4.png'
+
+
+const BettingSlider = ()=> {
+//   const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+
+
+  return (
+    <div className="swiper-slider-section">
+      <Swiper
+      loop={true}
+      modules={[FreeMode, Navigation, Thumbs]}
+      className="SwiperTop"
+      >
+        <SwiperSlide>
+          <img src={trend1}  alt="trend1"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend2}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend3}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend1}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend2}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend3}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend1}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend1}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend1}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend1}  alt="trend1" />
+        </SwiperSlide>
+      </Swiper>
+      <Swiper
+            style={{
+                "--swiper-navigation-color": "#fff",
+                "--swiper-pagination-color": "#fff",
+              }}
+        loop={true}
+        navigation={true}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="swiperBottom"
+      >
+        <SwiperSlide>
+          <img src={trend1}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend2}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend3}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend1}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend2}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend3}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend1}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend2}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend3}  alt="trend1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={trend4}  alt="trend1" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+
+export default BettingSlider;
