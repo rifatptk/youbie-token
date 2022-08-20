a60fec22b8ed06ad0bc01a8d02de5322ae7ad775
SohelTanbir <soheltanbir19@gmail.com>
2022-08-20 12:27:43 +0600

    added roadmap line and circle

--- src/components/RoadMap/RoadmapTwo.js
+++ src/components/RoadMap/RoadmapTwo.js
@@ -10,8 +10,16 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Pagination } from "swiper";
+import { useRef } from "react";
 
  function RoadMapTwo() {
+    const roadLineRef = useRef();
+
+const handleAnimation = ()=>{
+    console.log(roadLineRef.current.classList.contains("activeCircle"));
+}
+
+
   return (
     <div className="roadmap-two">
       <Swiper
@@ -21,53 +29,76 @@ import { Pagination } from "swiper";
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination]}
         className="mySwiper"
       >
         <SwiperSlide>
           <div className="roadmap-item">
-                <h4>Current State</h4>
-                <h5>May 2018</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus nesciunt cum impedit provident?</p>
+                <h4>Q1 </h4>
+                <div className="road-line activeSlide">
+                    <div onClick={handleAnimation} ref={roadLineRef} className="circle activeCircle"></div>
+                </div>
+                <ul>
+                    <li><span>✓</span>UI/UX Live Streaming Platform </li>
+                    <li><span>✓</span>Begin Development of Live Streaming Platform  </li>
+                    <li><span>✓</span>Begin Development of Live Streaming Platform  </li>
+                    <li><span>✓</span> Build Brand Identity  </li>
+                    <li><span>✓</span> Build Brand Identity  </li>
+                </ul>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="roadmap-item">
-                <h4 className="active-title">3 Months</h4>
-                <h5 className="active-sub-title">May - June 2018</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus nesciunt cum impedit provident? amet consectetur adid</p>
+                <h4>Q2 </h4>
+                <div className="road-line">
+                <div className="circle "></div>
+                </div>
+                <ul>
+                    <li><span>✓</span>Bug Testing For Live Streaming  </li>
+                    <li><span>✓</span>Complete Live Streaming Development </li>
+                    <li><span>✓</span>Cryptocurrency Mainnet Integration (BTC,LTC,ETH</li>
+                    <li><span>✓</span> Premium Subscription Development / Integration </li>
+                    <li><span>✓</span>UI/UX NFT Marketplace  </li>
+                    <li><span>✓</span>NFT Marketplace Development</li>
+                    <li><span>✓</span>Smart Contract Bug Testing (NFT Marketplace</li>
+                    <li><span>✓</span>Physical Marketplace UI/UX</li>
+                    <li><span>✓</span>Physical Marketplace UI/UX</li>
+                </ul>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="roadmap-item">
-                <h4>6 Months</h4>
-                <h5>June - October 2018</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus nesciunt cum impedit provident?</p>
+                <h4>Q3 </h4>
+                <div className="road-line">
+                <div className="circle"></div>
+                </div>
+                <ul>
+                    <li><span>✓</span>Physical Marketplace Developmen</li>
+                    <li><span>✓</span>Physical Marketplace Bug Testing / Integration  </li>
+                    <li><span>✓</span>Integrate Web3 Wallets (Metamask, Walletconnect) </li>
+                    <li><span>✓</span>Develop Youbie Token Smart Contract </li>
+                    <li><span>✓</span>Exchange Listings I <span> ☐ </span> Youbie Token Contract Audit </li>
+                    <li><span>✓</span>  ☐ Launch Youbie Token ☐</li>
+                    <li><span>✓</span>Marketing Campaign I  ☐ Host Youbie Gaming Tournament  </li>
+
+                </ul>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="roadmap-item">
-                <h4>1 Year</h4>
-                <h5>November 2018</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus nesciunt cum impedit provident?</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="roadmap-item">
-                <h4>Current State</h4>
-                <h5>May 2018</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus nesciunt cum impedit provident?</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="roadmap-item">
-                <h4>Current State</h4>
-                <h5>May 2018</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus nesciunt cum impedit provident?</p>
+                <h4>Q4 </h4>
+                  <div className="road-line">
+                  <div className="circle"></div>
+                  </div>
+                <ul>
+                    <li><span>✓</span> Marketing Campign II 
+                    ☐ Contract Top Streamers  </li>
+                    <li><span>✓</span>☐ Exchange Listings II</li>
+                </ul>
           </div>
         </SwiperSlide>
 
-       
+
+    
       </Swiper>
     </div>
   );
